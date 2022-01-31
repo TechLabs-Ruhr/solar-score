50df678d0cff5adbbdcf4190de270ce2e42e7617
Marian Reitzig <m.reitzig@yahoo.com>
2022-01-31 23:16:38 +0100

    Changes to test the python environment

--- prediction/examples/classic/datasets.py
+++ prediction/examples/classic/datasets.py
@@ -3,7 +3,7 @@ import statsmodels.datasets.co2 as co2
 def get_co2():
     """Small wrapper for getting and cleaning the CO2 data from statsmodels datasets."""
     
-    co2_data = co2.load().data
+    co2_data = co2.load(as_pandas=True).data
     co2_data = co2_data.fillna(co2_data.interpolate())
 
     return co2_data
