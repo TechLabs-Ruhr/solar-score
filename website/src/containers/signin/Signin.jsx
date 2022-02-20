8179d4f5c101a8778f4f02b4e152680e0647a6c0
Kat Weid <_Kathi-Weidemann_@web.de>
2022-02-20 16:20:57 +0100

    Update SignIn

--- website/src/containers/signin/Signin.jsx
+++ website/src/containers/signin/Signin.jsx
@@ -6,25 +6,7 @@ import { Link } from 'react-router-dom';
 
 const Signin = () => {
   return (<div className="solarscore__signin section__margin" id="signin">
-    <div className="solarscore__signin-content">
-        <h1 className="gradient__text">Sign Up here</h1>
-    </div>
-    <Router>
-    <div className="solarscore__navbar-sign">
-              <p><Link to='/login'>Login</Link></p>
-            </div>
-            <div className="solarscore__navbar-sign">
-              <Link to='/signup'>
-              <button type="button">Sign Up</button>
-              </Link>
-            </div>
-    <Routes>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/logout' element={<Logout/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-        </Routes>
-      </Router>
+    <Signup />
   </div>);
 };
 
