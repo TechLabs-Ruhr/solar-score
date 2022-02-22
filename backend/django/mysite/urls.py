7c22d1ba5cbdd73743fe0ab3f3d483fe880a1fc0
DeniseGrunert <97551635+DeniseGrunert@users.noreply.github.com>
2022-02-22 20:28:32 +0100

    Delete backend/django directory

--- backend/django/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-# mysite/urls.py
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-path('admin/', admin.site.urls),
-path('api/v1/users/', include('users.urls')),
-]
