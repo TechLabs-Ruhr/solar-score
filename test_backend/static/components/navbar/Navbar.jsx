883f8eca170ccba32f35ce93a904130b00cb1e98
Kat Weid <_Kathi-Weidemann_@web.de>
2022-03-09 20:20:54 +0100

    create folder statics and templates

---
+++ test_backend/static/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect, Fragment} from 'react';
+import logo from '../../assets/logo.svg';
+import './navbar.css';
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect (() => {
+    if (localStorage.getItem('token') !== null)
+    {
+      setIsAuth(true);
+    }
+  }, []);
+
+  return (
+    <div className="solarscore__navbar">
+      <div className="solarscore__navbar-links-logo">
+      <Link to='/'><img src ={logo} alt = "logo" /></Link>
+      </div>
+      {isAuth === true ? (
+          <Fragment>
+            {' '}
+            <div className="solarscore__navbar-sign">
+              <Link to='/logout'>Logout</Link>
+            </div>
+            <div className="solarscore__navbar-sign">
+              <Link to='/dashboard'>
+              <button type="button"> Dashboard</button>
+              </Link>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            {' '}
+            <div className="solarscore__navbar-sign">
+              <p><Link to="/login">Login</Link></p>
+            </div>
+            <div className="solarscore__navbar-sign">
+              <Link to ="/signup">
+              <button type="button">Sign Up</button>
+              </Link>
+            </div>
+          </Fragment>
+        )}
+        </div>
+        
+  ); 
+};
+
+export default Navbar;
---
+++ test_backend/static/views/auth/index.jsx
@@ -0,0 +1,3 @@
+export {default as Logout} from './logout/Logout';
+export {default as Login} from './login/Login';
+export {default as Signup} from './signup/Signup';
