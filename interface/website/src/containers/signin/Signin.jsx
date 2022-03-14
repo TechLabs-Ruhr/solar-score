b24d9580e79e3528c053883ae65feb1951f1cdcb
Kat Weid <_Kathi-Weidemann_@web.de>
2022-03-14 20:13:24 +0100

    Merge branch 'intro-Katharina' of https://github.com/TechLabs-Dortmund/solar-score into intro-Katharina

---
+++ interface/website/src/containers/signin/Signin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './signin.css';
+import {Signup, Login, Logout, Dashboard} from '../../views/auth/index';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Signin = () => {
+  return (<div className="solarscore__signin section__margin" id="signin">
+    <Signup />
+  </div>);
+};
+
+export default Signin;
---
+++ interface/website/src/index.js
@@ -0,0 +1,26 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import './index.css';
+import  Logout  from './views/auth/logout/Logout';
+import SignupPage from './pages/signup/SignupPage';
+import LoginPage from './pages/login/LoginPage';
+import SignupPageOne from './pages/signup/SignupPageOne';
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="" element={<App />} />
+    <Route path="login" element={<LoginPage />} />
+    <Route path="signup" element={<SignupPage />} />
+    <Route path="signup1" element={<SignupPageOne />} />
+    <Route path="logout" element={<Logout />} />
+  </Routes>
+  </BrowserRouter>,
+  rootElement
+);
