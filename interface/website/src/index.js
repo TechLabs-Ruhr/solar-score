543954b546c9f58ec2a2939334bdba2bd5b90d69
Inka Stinn <inka.stinn@smartsim.energy>
2022-03-20 16:31:58 +0100

    frontend routes

--- interface/website/src/index.js
+++ interface/website/src/index.js
@@ -8,12 +8,14 @@ import LogoutPage from './pages/logout/LogoutPage';
 import SignupPage from './pages/signup/SignupPage';
 import LoginPage from './pages/login/LoginPage';
 import DashboardPage from './pages/dashboard/DashboardPage';
+import App from './App';
 
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
   <Routes>
+    <Route path="" element={<App />} />
     <Route path="login" element={<LoginPage />} />
     <Route path="signup" element={<SignupPage />} />
     <Route path="dashboard" element={<DashboardPage />} />
