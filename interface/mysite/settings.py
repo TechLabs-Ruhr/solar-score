1d90a9c6b97bbb97f4e80856cb4037210c90b7ef
Kat Weid <_Kathi-Weidemann_@web.de>
2022-04-03 16:48:34 +0200

    remove renderer classes

--- interface/mysite/settings.py
+++ interface/mysite/settings.py
@@ -175,10 +175,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',    
         #'rest_framework.authentication.BasicAuthentication',    
         ],
-    'DEFAULT_RENDERER_CLASSES': [
-            'rest_framework.renderers.BrowsableAPIRenderer',
-            'rest_framework.renderers.JSONRenderer',
-        ],
+# #    """  'DEFAULT_RENDERER_CLASSES': [
+# #             'rest_framework.renderers.BrowsableAPIRenderer',
+# #             'rest_framework.renderers.JSONRenderer', """
+#         ],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
 }
